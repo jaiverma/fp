ff9e8c7f24762bc10a8f8af715898d032e3c4a0b
Jai Verma <jai2.verma@outlook.com>
2021-08-21 14:05:22 +0530

    Applying ocamlformat

--- dailybyte/19-08-2021/main.ml
+++ dailybyte/19-08-2021/main.ml
@@ -2,18 +2,19 @@ open Base
 open Stdio
 
 let decode s =
-    let rec decode_impl s acc =
-        match s with
-        | n :: c :: xs ->
-            let n = Int.of_string n in
-            decode_impl xs @@ acc @ List.init n ~f:(fun _ -> c)
-        | [] -> acc
-        | _ -> failwith "..."
-    in
-
-    let chars = String.to_list s |> List.map ~f:String.of_char in
-    String.concat ~sep:"" @@ decode_impl chars []
+  let rec decode_impl s acc =
+    match s with
+    | n :: c :: xs ->
+      let n = Int.of_string n in
+      decode_impl xs @@ acc @ List.init n ~f:(fun _ -> c)
+    | [] -> acc
+    | _ -> failwith "..."
+  in
+  let chars = String.to_list s |> List.map ~f:String.of_char in
+  String.concat ~sep:"" @@ decode_impl chars []
+;;
 
 let () =
-    let ans = decode "3a2b1c" in
-    printf "%s\n" ans
+  let ans = decode "3a2b1c" in
+  printf "%s\n" ans
+;;
